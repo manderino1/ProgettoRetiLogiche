149179546957210c811ec0e05702db4dc2934f8d
Unknown <marco.mandelli212@gmail.com>
2020-02-12 12:28:42 +0100

    First process example (no registers)

--- progetto_reti.srcs/sources_1/new/main.vhd
+++ progetto_reti.srcs/sources_1/new/main.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -46,6 +46,85 @@ end project_reti_logiche;
 architecture Behavioral of project_reti_logiche is
 
 begin
-
-
+-- Processo di start
+    process(i_start)
+        variable addr : unsigned(7 downto 0);
+        variable wbPresent : STD_LOGIC;
+        variable wbBlock : STD_LOGIC_VECTOR(2 downto 0);
+        variable wbOffset : STD_LOGIC_VECTOR(3 downto 0);
+    begin
+        if i_start = '0' then
+            o_done <= '0';
+        end if;
+        if i_start = '1' then
+            wbPresent := '0';
+            o_en <= '1';
+            o_we <= '0';
+            -- Read the input
+            o_address <= "0000000000001000";
+            addr := unsigned(i_data);
+            -- Check for word block in every input
+            for i in 0 to 7 loop
+                case i is
+                    when 0 =>
+                        o_address <= "0000000000000000";
+                        wbBlock := "000";
+                    when 1 =>
+                        o_address <= "0000000000000001";
+                        wbBlock := "001";
+                    when 2 =>
+                        o_address <= "0000000000000010";
+                        wbBlock := "010";
+                    when 3 =>
+                        o_address <= "0000000000000011";
+                        wbBlock := "011";
+                    when 4 =>
+                        o_address <= "0000000000000100";
+                        wbBlock := "100";
+                    when 5 =>
+                        o_address <= "0000000000000101";
+                        wbBlock := "101";
+                    when 6 =>
+                        o_address <= "0000000000000110";
+                        wbBlock := "110";
+                    when 7 =>
+                        o_address <= "0000000000000111";
+                        wbBlock := "111";
+                end case;
+                if unsigned(i_data) = addr then
+                    -- Exists in a wb
+                    wbPresent := '1';
+                    wbOffset := "0001";
+                    exit;
+                end if;
+                if unsigned(i_data) = addr+1 then
+                    -- Exists in a wb
+                    wbPresent := '1';
+                    wbOffset := "0010";
+                    exit;
+                end if;
+                if unsigned(i_data) = addr+2 then
+                    -- Exists in a wb
+                    wbPresent := '1';
+                    wbOffset := "0100";
+                    exit;
+                end if;
+                if unsigned(i_data) = addr+3 then
+                    -- Exists in a wb
+                    wbPresent := '1';
+                    wbOffset := "1000";
+                    exit;
+                end if;
+            end loop;
+            if wbPresent = '0' then
+                o_data <= '0' & STD_LOGIC_VECTOR(addr);
+            else
+                -- 1 + WZ in binario + Offset in one hot
+                o_data <= '1' & wbBlock & wbOffset;
+            end if;
+            o_en <= '1';
+            o_we <= '1';
+            o_done <= '1';
+        end if;           
+    end process;
 end Behavioral;
