3d31f6b12a6164e7de6bd26d5a8b64c4f714fe43
Unknown <marco.mandelli212@gmail.com>
2020-03-02 16:46:16 +0100

    Fixed done

--- progetto_reti.sim/sim_1/behav/xsim/xsim.dir/project_tb_behav/obj/xsim_1.c
+++ progetto_reti.sim/sim_1/behav/xsim/xsim.dir/project_tb_behav/obj/xsim_1.c
@@ -60,8 +60,8 @@ const int NumRelocateId= 6;
 void relocate(char *dp)
 {
 	iki_relocate(dp, "xsim.dir/project_tb_behav/xsim.reloc",  (void **)funcTab, 6);
-	iki_vhdl_file_variable_register(dp + 1056576);
-	iki_vhdl_file_variable_register(dp + 1056632);
+	iki_vhdl_file_variable_register(dp + 1054784);
+	iki_vhdl_file_variable_register(dp + 1054840);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */
